a92ca3e623d9ffa488e9ea4bd5492a743013e565
yamato1001 <yamato1001apa@i.softbank.jp>
2021-03-05 03:36:24 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_parents
@@ -9,5 +9,11 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '1234'
+    end
   end
 end
